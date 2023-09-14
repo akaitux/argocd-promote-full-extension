550753e59b329c8bd9faa91b007b244ed2759479
Mikhail Bocharov <bocharov.mihail@gmail.com>
2023-09-14 23:24:11 +0300

    Argo-rollouts

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -3,6 +3,7 @@ import {ErrorNotification, NotificationType, SlidingPanel} from 'argo-ui';
 
 export const Extension = (props: { tree: any; resource: any}) => {
     <div>Hello YOYO {props.resource.metadata.name}!</div>
+    console.log("YOOOOOOOO")
 };
 
 export const component = Extension;
