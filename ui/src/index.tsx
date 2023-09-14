96af07651d587a0a3ad499a60cc81d214a2c47c7
Mikhail Bocharov <bocharov.mihail@gmail.com>
2023-09-14 23:49:45 +0300

    Argo-rollouts

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -3,20 +3,16 @@ import {ErrorNotification, NotificationType, SlidingPanel} from 'argo-ui';
 
 export const Extension = (props: { tree: any; resource: any}) => {
     <div>Hello YOYO {props.resource.metadata.name}!</div>
-    console.log("YOOOOOOOO")
 };
 
 export const component = Extension;
 
 // Register the component extension in ArgoCD
 ((window: any) => {
-  window?.extensionsAPI?.registerResourceExtension(
+  window?.extensionsAPI?.registerSystemLevelExtension(
     component,
-    "*",
-    "Rollout",
-    "Metrics",
-    { icon: "fa fa-chart-area" }
+    "Test Ext",
+    "/hello",
+    "fa-flask"
   );
-  window?.extensionsAPI?.registerResourceExtension(component, '', 'Pod', 'Metrics', { icon: "fa fa-chart-area" });
-  window?.extensionsAPI?.registerResourceExtension(component, '*', 'Deployment', 'Metrics', { icon: "fa fa-chart-area" });
 })(window);
